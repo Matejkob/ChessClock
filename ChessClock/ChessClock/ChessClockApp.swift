525c23729047ecb2a30e559ac8ddbc057f0b65cf
Mateusz Bąk <mateusz.bak@x-kom.pl>
2021-02-25 15:55:48 +0100

    Create new project

---
+++ ChessClock/ChessClock/ChessClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessClockApp.swift
+//  ChessClock
+//
+//  Created by Mateusz Bąk on 25/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct ChessClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
